6b9ba2e383e3846e2cd80fdee6e523670cf07d14
Adonis Joe <emekajoe5@gmail.com>
2023-08-14 10:37:15 +0100

    changes on unwanted pages/components, routing and Api fetching
    unwanted pages are now deleted. Routing now implemeted, svg files now have the required logos including the backgrounds matching the logos. Some information now coming from the api provided except the ambigious ones that need further clarifications.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+// import Home from './pages/Home';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './index.css';
+import Tiktok from './pages/tiktok/Tiktok';
+import Facebook from './pages/facebook/Facebook';
+import Youtube from './pages/youtube/Youtube';
+import Home from './pages/Home';
+import Instagram from './pages/instagram/Instagram';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const [
+    facebookPostLikes,
+    facebookPageLikes,
+    instagramPostLikes,
+    instagramFollowers,
+    youtubeSubscribers,
+    youtubeViews,
+    tiktokVideoLikes,
+    tiktokFollowers,
+  ] = data;
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://api.sheety.co/06def408e74850aef0fbd22a79539f9f/ldServices/services'
+      ); // Replace with your API endpoint
+      const jsonData = await response.json();
+      setData(jsonData.services);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route
+        path='/facebook'
+        element={
+          <Facebook
+            facebookPostLikes={facebookPostLikes}
+            facebookPageLikes={facebookPageLikes}
+          />
+        }
+      />
+      <Route
+        path='/tiktok'
+        element={
+          <Tiktok
+            tiktokVideoLikes={tiktokVideoLikes}
+            tiktokFollowers={tiktokFollowers}
+          />
+        }
+      />
+      <Route
+        path='/youtube'
+        element={
+          <Youtube
+            youtubeSubscribers={youtubeSubscribers}
+            youtubeViews={youtubeViews}
+          />
+        }
+      />
+      <Route
+        path='/instagram'
+        element={
+          <Instagram
+            instagramPostLikes={instagramPostLikes}
+            instagramFollowers={instagramFollowers}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
