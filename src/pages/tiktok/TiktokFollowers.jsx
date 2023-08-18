d7562c3844b7d201418f842ba3af6249eafece52
Adonis Joe <emekajoe5@gmail.com>
2023-08-18 23:41:31 +0100

    routing implemented
    Pages in tiktok now linked to seperate pages e.g Tiktok followers page and tiktok video likes page

---
+++ src/pages/tiktok/TiktokFollowers.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TiktokFollowers = () => {
+  return <div>TiktokFollowers</div>;
+};
+
+export default TiktokFollowers;
---
+++ src/pages/tiktok/TiktokLikes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TiktokLikes = () => {
+  return <div>TiktokLikes</div>;
+};
+
+export default TiktokLikes;
